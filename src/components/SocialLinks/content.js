63f4af0d89bbc05a03414255b59cd0d8728c5f87
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2020-06-16 20:47:48 -0300

    Created SocialLinks component

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,25 @@
+const handle = "felipebbarbosa"
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`
+  },
+  {
+    label: "Youtube",
+    url: `https://www.youtube.com/${handle}`
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/${handle}`
+  },
+  {
+    label: "Unsplash",
+    url: "#"
+  },
+]
+
+export default links
