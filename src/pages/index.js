44415638406ffa417d33d4aded2361041c6619f1
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2020-06-17 21:42:28 -0300

    Structured the PostItem data

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,15 @@ import PostItem from "../components/PostItem"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
+    <PostItem 
+      slug="/about/"
+      background="red"
+      category="Misc"
+      date="17 de Junho de 2020"
+      timeToRead="10"
+      title="Diga não ao Medium"
+      description="Algumas razões para você...."
+    />
   </Layout>
 )
 
