7e564aa495a4cd67ebc1ad0adffaeafd844dfe55
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2020-07-16 20:43:43 -0300

    Developed the animation between links transitions using gatsby-plugin-transition-link

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,6 +2,9 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Avatar from "../Avatar"
+
+import getThemeColor from "../../utils/getThemeColor"
+
 import * as S from "./styled"
 
 const Profile = () => {
@@ -24,7 +27,7 @@ const Profile = () => {
 
   return(
       <S.ProfileWrapper>
-        <S.ProfileLink>
+        <S.ProfileLink to="/" cover direction="left" bg={getThemeColor()} duration={0.6}>
           <Avatar />
           <S.ProfileAuthor>
             {title}
