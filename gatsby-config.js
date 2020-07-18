e1a79d5415df715fdc6837999623fd09e77e2db3
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2020-07-18 17:34:40 -0300

    Adjusted the SEO settings including sitemap and robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     position: `Programador`,
     description: `A blog about frontend development and other cool stuff.`,
     author: `@felipebbarbosa`,
+    siteUrl: `https://felipebbarbosa.com.br`
   },
   plugins: [
     `gatsby-plugin-transition-link`,
@@ -83,6 +84,7 @@ module.exports = {
         //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
